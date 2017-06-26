7b70da0f665ddb0a3a59d27fea9e67f5d1046875
Nate Mara natemara@gmail.com <nathan.mara@kroger.com>
2017-06-26 12:38:11 -0400

    add strict evaluation for linear memory complexity

--- src/Monte.hs
+++ src/Monte.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns #-}
 module Monte where
 
 import Data.List (map, length, foldl')
@@ -6,18 +7,18 @@ import qualified System.Random as R
 
 type Point = (Double, Double)
 
-instance (Random a, Random b) => Random (a, b) where 
+instance (Random a, Random b) => Random (a, b) where
 --   -- RandomGen g => (a, a) -> g -> (a, g)
-  randomR ((loa, lob), (hia, hib)) gen = 
-    let 
+  randomR ((loa, lob), (hia, hib)) gen =
+    let
       (first, newGen) = R.randomR (loa, hia) gen
       (second, newGen') = R.randomR (lob, hib) newGen
     in
       ((first, second), newGen')
 
 --   -- RandomGen g => g -> (a, g)
-  random gen = 
-    let 
+  random gen =
+    let
       (first, newGen) = R.random gen
       (second, newGen') = R.random newGen
     in
@@ -31,23 +32,23 @@ getRandom gen =
     num : getRandom newGen
 
 estimatePi :: Int -> IO Double
-estimatePi limit = fmap (* 4) $ fmap averageWeights $ fmap (take limit) (fmap getRandom R.getStdGen)
+estimatePi limit = fmap (* 4) $ fmap averageWeights' $ fmap (take limit) (fmap getRandom R.getStdGen)
 
 weight :: Point -> Double
-weight (x, y) = 
-  let distance = sqrt $ x * x + y * y   
+weight (x, y) =
+  let distance = sqrt $ x * x + y * y
   in
     if distance <= 1 then 1 else 0
 
 averageWeights :: [Point] -> Double
 averageWeights points =
   let weightedValues = map weight points
-  in 
+  in
     sum weightedValues / fromIntegral (length weightedValues)
 
 averageWeights' :: [Point] -> Double
-averageWeights' points = 
-  let (length, sum) = foldl' (\ (len, sum) point -> (len + 1, sum + weight point)) (0, 0) points
+averageWeights' points =
+  let (!length, !sum) = foldl' (\ (!len, !sum) point -> (len + 1, sum + weight point)) (0, 0) points
   in sum / length
 
 approximatePi :: [Point] -> Double
